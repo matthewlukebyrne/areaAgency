c7bf65f1d002a7fc8674b5aa571b6ee3b7b5b571
Matt <matthewlukebyrne@gmail.com>
2020-06-04 18:12:53 +0100

    linking of static folder to bring in Bootstrap files JS and CSS

--- areaAgency/settings.py
+++ areaAgency/settings.py
@@ -129,5 +129,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# This is place to declare your static files in the main areaAgency folder
 
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'areaAgency/static')
+]
---
+++ areaAgency/static/js/main.js
@@ -0,0 +1,3 @@
+// A Function for retriving the current year at the bottom of the page (2020!)
+const date = new Date();
+document.querySelector('.year').innerHTML = date.getFullYear();
