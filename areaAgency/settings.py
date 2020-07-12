89a5ca10a38d8314c199e946ba726b1b74eea2a6
Matt <matthewlukebyrne@gmail.com>
2020-07-12 18:40:14 +0100

    more updates (issues)

--- areaAgency/settings.py
+++ areaAgency/settings.py
@@ -164,4 +164,10 @@ MESSAGE_TAGS = {
 # EMAIL_HOST_PASSWORD = '********'
 # EMAIL_USE_TLS = True
 
+#Push server local_settings
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 
